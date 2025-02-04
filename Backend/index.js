a38c5465c82c3fb12d46dfca59a186896b1490b1
Madhav Solanki <madhav09solanki@gmail.com>
2025-02-04 12:30:25 +0530

    Url Changed

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["https://jobportal-3631.onrender.com"],
   credentials: true,
 };
 
