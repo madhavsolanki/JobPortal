bd9cf5783e804600ef6ce0548489a3c1787d8c7f
Madhav Solanki <madhav09solanki@gmail.com>
2025-02-04 13:43:50 +0530

    Deploy

--- Backend/Frontend/src/utils/data.js
+++ Backend/Frontend/src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "https://jobportal-1-jg2c.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://jobportal-1-jg2c.onrender.com/job";
-export const APPLICATION_API_ENDPOINT = "https://jobportal-1-jg2c.onrender.com/api/application";
-export const COMPANY_API_ENDPOINT = "https://jobportal-1-jg2c.onrender.com/api/company";
+export const USER_API_ENDPOINT = "https://jobportal-v09k.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://jobportal-v09k.onrender.com/job";
+export const APPLICATION_API_ENDPOINT = "https://jobportal-v09k.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://jobportal-v09k.onrender.com/api/company";
--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: ["https://jobportal-1-jg2c.onrender.com"],
+  // origin: ["*"],
   credentials: true,
 };
 
